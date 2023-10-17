24dce6212988c85e5da7a3ce8f8100ef5c62513a
Gunel Farajli <gunel.farajli@kcl.ac.uk>
2023-10-17 18:35:30 +0100

    Find frequencies

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,12 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
-
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        item = str(item)
+        if item in frequencies:
+            frequencies[item] = frequencies[item] + 1
+        else:
+            frequencies[item] = 1
     return frequencies
